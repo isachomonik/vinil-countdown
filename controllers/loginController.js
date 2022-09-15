eb9e89fcbb6912f58c579dd492accc509606c44d
Ligia Felix <ligiafelix5@gmail.com>
2022-09-15 11:22:06 -0300

    correcao  loggin

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,10 +1,10 @@
 // Criacao objeto Literal para armazenar as funcoes de controller de cada endereco indicado pelo roteador para controle.
 
-const logginController = {
+const loginController = {
 
     index: (req, res)=>{
         res.render('loggin.ejs');
     }
 }
 
-module.exports = logginController;
+module.exports = loginController;
--- routers/loginRouter.js
+++ routers/loginRouter.js
@@ -1,5 +1,5 @@
 // Bloco para uso de Rotas do projeto (Requisicao Controller e Indicacao Caminho)
-var MinhaContaController = require('../controllers/logginController');
+var MinhaContaController = require('../controllers/loginController');
 
 // 1. Requizicao do modulo NPM Express
 var express = require('express');
@@ -7,7 +7,7 @@ var express = require('express');
 // 2 Config. Modulo express para chamar a funcao roteador
 var router = express.Router();
 
-router.get('/', logginController.index);
+router.get('/', loginController.index);
 
 //3. Exportando o Modulo de roteamento para uso nas funcoes de Controller
 module.exports = router;
