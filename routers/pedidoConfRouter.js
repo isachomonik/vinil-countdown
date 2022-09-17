64f4e257e558134cffff4ab79b91c6e7c899ca99
Sthefani Gomes <sthefani.gomess@hotmail.com>
2022-09-17 17:51:06 -0300

    HTML da pag Pedido Confirmado

---
+++ routers/pedidoConfRouter.js
@@ -0,0 +1,9 @@
+var PedidoConfController = require('../controllers/PedidoConfController');
+
+var express = require('express');
+
+var router = express.Router();
+
+router.get('/', PedidoConfController.index);
+
+module.exports = router;
