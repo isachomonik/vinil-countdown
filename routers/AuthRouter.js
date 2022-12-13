f5af9701c6118aae00184b7fcb8cfd58bbb89573
Isac <isachomonik@outlook.com>
2022-12-13 10:22:48 -0300

    form cadastro

--- routers/AuthRouter.js
+++ routers/AuthRouter.js
@@ -1,7 +1,7 @@
-const AuthRouter = require('express').Router();
-const AuthController = require('../controllers/AuthController')
-AuthRouter.get('/login', AuthController.showEntrar);
-AuthRouter.post('/login', AuthController.login);
-AuthRouter.post('/add', AuthController.add); //oq faz esse add? addCart?
+const AuthRouter = require("express").Router();
+const AuthController = require("../controllers/AuthController");
+AuthRouter.get("/login", AuthController.showEntrar);
+AuthRouter.post("/login", AuthController.login);
+AuthRouter.post("/add", AuthController.add); //oq faz esse add? addCart?
 
 module.exports = AuthRouter;
